07ae83eb553b7dcb3c0fac343061c2c305690ead
rvreeland <rebeccavreeland@gmail.com>
2015-02-16 20:11:55 -0800

    add polling for new messages and handle escaping

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -4,15 +4,21 @@ var messages = [];
 
 
 //Display Code
-var displayMessages = function () {
+var displayMessages = function (undisplayedMessages) {
   var messageContainer = $('.messages');
-  _.each(messages, function (message) {
-    var user = '<span class="username">' + message.username + '</span>';
-    var time = message.createdAt;
-    var timestamp = '<span class="timestamp">' + time.toLocaleDateString() + ' ' + time.toLocaleTimeString() + '</span>';
-    var messageHtml = '<div class="message-text">' + message.text + '</div>';
-    var fullMessage = $('<div class="chat">' + user + ' ' + timestamp + messageHtml + '</div>');
-    messageContainer.append(fullMessage);
+  _.each(undisplayedMessages, function (message) {
+    var $user = $('<span class="username"></span>');
+    $user.text(message.username + ' ');
+    var time = message.date;
+    var $timestamp = $('<span class="timestamp"></span>');
+    $timestamp.text(time.toLocaleDateString() + ' ' + time.toLocaleTimeString());
+    var $messageHtml = $('<div class="message-text"></div>');
+    $messageHtml.text(message.text);
+    var $fullMessage = $('<div class="chat"></div>');
+    $fullMessage.append($user);
+    $fullMessage.append($timestamp);
+    $fullMessage.append($messageHtml);
+    messageContainer.prepend($fullMessage);
   });
 };
 
@@ -22,25 +28,53 @@ var displayMessages = function () {
 //  { results:
 //      [ { keys are: createdAt, objectId, roomname, text, updatedAt, username}, ...]
 //  }
-$.get(url, function (response) {
-  console.log(response);
-  var results = _.filter(response.results, function(message) {
-    return message.text !== undefined && message.username !== undefined;
-  });
-  _.each(results, function (message) {
-    message.createdAt = new Date(message.createdAt);
-  });
-  results.sort(function (a, b) {
-    if (a.createdAt > b.createdAt) {
-      return -1;
-    } else if (a.createdAt < b.createdAt) {
-      return 1;
-    } else {
-      return 0;
+//  restrict with url + '?' + $.param({where: {createdAt: {__type: 'Date', iso: message.createdAt}}, ... }})
+
+var getMessages = function() {
+  var requestURL = url;
+  var getMessagesFrom = messages.length > 0
+                          ? messages[0].createdAt
+                          : new Date(Date.now() - 2 * 24 * 60 * 60 * 1000).toISOString();
+  requestURL = requestURL + '?' + $.param({
+    where: {
+      createdAt: {
+        $gt: {
+          __type: 'Date',
+          iso: getMessagesFrom
+        }
+      }
     }
   });
-  messages = messages.concat(results);
-  displayMessages();
-});
+  $.get(requestURL, function (response) {
+    // console.log(response);
+    var results = _.filter(response.results, function(message) {
+      return message.text !== undefined && message.username !== undefined;
+    });
+    _.each(results, function (message) {
+      message.date = new Date(message.createdAt);
+    });
+    results.sort(function (a, b) {
+      if (a.date < b.date) {
+        return -1;
+      } else if (a.date > b.date) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+    messages = results.concat(messages);
+    displayMessages(results);
+  });
+  // setTimeout(getMessages, 5000);
+};
+
+getMessages();
+
+
+
+
+
+
+
 
 
